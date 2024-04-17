fbc2968cf7e06ee2a964af985a760823e5140c04
Brandon Corey <bcorey3660@gmail.com>
2024-04-17 02:59:46 -0400

    remove unecessary image identifiers
    Co-authored-by: Allison-Embrey <allilearns@gmail.com>
    Co-authored-by: Gianni-Barber <giannibarber@protonmail.com>
    Co-authored-by: Tyler-Wenzel <tyler.gh3@gmail.com>

--- src/components/DashboardPreview.tsx
+++ src/components/DashboardPreview.tsx
@@ -1,12 +1,12 @@
 export default function DashBoardPreview(): JSX.Element {
   return (
-    <div className="flex flex-col justify-center items-center gap-4 py-10">
+    <div className="flex flex-col bg-white dark:bg-base justify-center items-center gap-4 py-10">
       <h2>Custom React Dashboard to display event and user insights</h2>
       <img
         src={require("/static/img/dataloaf-dashboard.png").default}
         alt="Image of Dataloaf Dashboard UI"
         width="1300"
-        className="rounded-lg"
+        className="rounded-lg border border-white"
       />
       <caption>
         Provides ability to select an event type, an aggregation type, and
