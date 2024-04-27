46ad49184c02e74e48d6b8244ba02282c82efc9c
Brandon Corey <bcorey3660@gmail.com>
2024-04-27 01:51:22 -0400

    add more pronounced horizontal borders on sdk and cli preview
    Co-authored-by: Allison-Embrey <allilearns@gmail.com>
    Co-authored-by: Gianni-Barber <giannibarber@protonmail.com>
    Co-authored-by: Tyler-Wenzel <tyler.gh3@gmail.com>

--- src/components/CliPreview.tsx
+++ src/components/CliPreview.tsx
@@ -1,6 +1,6 @@
 export default function CliPreview(): JSX.Element {
   return (
-    <div className="-z-20 flex md:flex-row flex-col justify-center items-center gap-16 py-20 px-10 bg-zinc-100 dark:bg-zinc-900">
+    <div className="-z-20 flex md:flex-row flex-col border-y border-zinc-300 dark:border-zinc-950 justify-center items-center gap-16 py-20 px-10 bg-zinc-100 dark:bg-zinc-900">
       <div className="bg-[#1d1d2d] p-2 rounded-md border border-accent drop-shadow-xl">
         <video loop autoPlay playsInline muted width="900">
           <source
--- src/components/sdkPreview.tsx
+++ src/components/sdkPreview.tsx
@@ -4,7 +4,7 @@ import Link from "@docusaurus/Link";
 
 export function SDKPreview(): JSX.Element {
   return (
-    <div className="bg-zinc-100 dark:bg-zinc-900 flex flex-col md:flex-row justify-center items-center gap-20 py-20 px-10">
+    <div className="bg-zinc-100 border-y border-zinc-300 flex flex-col md:flex-row justify-center items-center gap-20 py-20 px-10 dark:border-zinc-950 dark:bg-zinc-900">
       <div>
         <h3>Use the SDK for event and user generation</h3>
         <ul>
