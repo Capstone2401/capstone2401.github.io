22b6065e70bc6f8c370e70d2585109a6bb178f93
Brandon Corey <bcorey3660@gmail.com>
2024-05-01 18:02:27 -0400

    update dark mode base to be darker

--- src/components/CliPreview.tsx
+++ src/components/CliPreview.tsx
@@ -1,6 +1,6 @@
 export default function CliPreview(): JSX.Element {
   return (
-    <div className="-z-20 flex md:flex-row flex-col dark:border-none justify-center items-center gap-16 py-20 px-10 bg-zinc-100 dark:bg-[#212223]">
+    <div className="-z-20 flex md:flex-row flex-col dark:border-none justify-center items-center gap-16 py-20 px-10 bg-zinc-100 dark:bg-zinc-900">
       <div className="bg-[#1d1d2d] p-2 rounded-md border border-accent drop-shadow-xl">
         <video loop autoPlay playsInline muted width="900">
           <source
--- src/components/sdkPreview.tsx
+++ src/components/sdkPreview.tsx
@@ -4,7 +4,7 @@ import Link from "@docusaurus/Link";
 
 export function SDKPreview(): JSX.Element {
   return (
-    <div className="bg-zinc-100 flex flex-col md:flex-row justify-center items-center gap-20 py-20 px-10 dark:bg-[#212223]">
+    <div className="bg-zinc-100 flex flex-col md:flex-row justify-center items-center gap-20 py-20 px-10 dark:bg-zinc-900">
       <div>
         <h3>Use the SDK for event and user generation</h3>
         <ul>
