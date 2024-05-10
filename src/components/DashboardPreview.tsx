ba7bf504a44aad709a9b11da230dd1fe209ecd3c
Brandon Corey <bcorey3660@gmail.com>
2024-05-10 02:33:45 -0400

    add carousel of new assets to landing page
    Co-authored-by: Allison-Embrey <allilearns@gmail.com>
    Co-authored-by: Gianni-Barber <giannibarber@protonmail.com>
    Co-authored-by: Tyler-Wenzel <tyler.gh3@gmail.com>

--- src/components/DashboardPreview.tsx
+++ src/components/DashboardPreview.tsx
@@ -1,17 +1,60 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.css";
+import dataloafDashboardImage from "/static/img/dataloaf-dashboard-final.png";
+import dataloafFeaturesImage from "/static/img/dataloaf-dashboard-features-final.png";
+
 export default function DashBoardPreview(): JSX.Element {
+  const thumbs = [
+    dataloafDashboardImage,
+    dataloafFeaturesImage,
+    dataloafFeaturesImage,
+  ];
+
+  const renderThumbs = () => {
+    return thumbs.map((thumb, index) => (
+      <img
+        src={thumb}
+        alt="thumbnail for dataloaf dashboard carousel"
+        key={index}
+      />
+    ));
+  };
+
   return (
     <div className="flex flex-col justify-center items-center gap-4 py-20 px-10">
       <h2>React Dashboard to display event and user insights</h2>
-      <img
-        src={require("/static/img/dataloaf-dashboard-new.png").default}
-        alt="Image of Dataloaf Dashboard UI"
-        className="rounded-lg border border-accent"
-        width={1306}
-      />
-      <p>
-        Provides ability to select an event type, an aggregation type, and
-        filter based on both user and event attributes
-      </p>
+      <Carousel className="max-w-[1308px] w-full" renderThumbs={renderThumbs}>
+        <div>
+          <img
+            src={dataloafDashboardImage}
+            alt="Image of Dataloaf Dashboard UI"
+            className="rounded-lg border border-accent"
+          />
+          <p className="legend">
+            Provides ability to select an event type, an aggregation type, and
+            filter based on both user and event attributes
+          </p>
+        </div>
+        <div>
+          <img
+            src={dataloafFeaturesImage}
+            alt="Image of Dataloaf Dashboard UI"
+            className="rounded-lg border border-accent"
+          />
+          <p className="legend">
+            Provides multiple straight forwards visualizations such as line,
+            area, and bar chart
+          </p>
+        </div>
+        <div>
+          <video loop autoPlay playsInline muted className="rounded-md">
+            <source
+              src={require("/static/img/loaf-dashboard-demo.mp4").default}
+            ></source>
+          </video>
+          <p className="legend">Brief demo showing off the dashboard</p>
+        </div>
+      </Carousel>
     </div>
   );
 }
