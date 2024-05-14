5f64bceaaeddf5d4d24d092c112115c1c1e59ebe
Brandon Corey <bcorey3660@gmail.com>
2024-05-14 02:10:04 -0400

    add dataloaf infra to carousel

--- src/components/DashboardPreview.tsx
+++ src/components/DashboardPreview.tsx
@@ -2,12 +2,14 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.css";
 import dataloafDashboardImage from "/static/img/dataloaf-dashboard-final.png";
 import dataloafFeaturesImage from "/static/img/dataloaf-dashboard-features-final.png";
+import dataloafInfra from "/static/img/dataloaf-infra.png";
 
 export default function DashBoardPreview(): JSX.Element {
   const thumbs = [
     dataloafDashboardImage,
     dataloafFeaturesImage,
     dataloafFeaturesImage,
+    dataloafInfra,
   ];
 
   const renderThumbs = () => {
@@ -23,14 +25,14 @@ export default function DashBoardPreview(): JSX.Element {
   return (
     <div className="flex flex-col justify-center items-center gap-4 py-20 px-10">
       <h2 className="text-center xl:text-3xl lg:text-3xl text-2xl">
-        React Dashboard to display event and user insights
+        Real-time Dashboard powered by AWS infrastructure
       </h2>
       <Carousel className="max-w-[1308px] w-full" renderThumbs={renderThumbs}>
         <div>
           <img
             src={dataloafDashboardImage}
             alt="Image of Dataloaf Dashboard UI"
-            className="rounded-lg border border-accent"
+            className="rounded-lg border border-accent antialiased"
           />
           <p className="legend hidden md:block lg:block xl:block">
             Select an event type, an aggregation type, and filter based on both
@@ -41,13 +43,14 @@ export default function DashBoardPreview(): JSX.Element {
           <img
             src={dataloafFeaturesImage}
             alt="Image of Dataloaf Dashboard UI"
-            className="rounded-lg border border-accent"
+            className="rounded-lg border border-accent antialiased"
           />
           <p className="legend hidden md:block lg:block xl:block">
             Provides simple to understand visualizations such as line, area, and
             bar chart
           </p>
         </div>
+
         <div>
           <video loop autoPlay playsInline muted className="rounded-md">
             <source
@@ -56,6 +59,16 @@ export default function DashBoardPreview(): JSX.Element {
           </video>
           <p className="legend hidden md:block lg:block xl:block">
             Brief demo showing off the dashboard
+          </p>
+        </div>
+        <div>
+          <img
+            src={dataloafInfra}
+            alt="Image of Dataloaf Dashboard UI"
+            className="rounded-lg border border-accent"
+          />
+          <p className="legend hidden md:block lg:block xl:block">
+            The current architecture of DataLoaf
           </p>
         </div>
       </Carousel>
